bbca6951dc49729c099ee0cb73265279607567f2
윤민석 <ykmvm147@naver.com>
2024-02-28 23:03:16 +0900

    feat: js파일 연동 및 화면별 기본값 세팅 완료

---
+++ js/blank.js
@@ -0,0 +1,30 @@
+(() => {
+  const sceneInfo = [
+    {
+      // 0
+      type: "sticky",
+      heightNum: 5,
+      scrollHeight: 0,
+    },
+    {
+      // 1
+      type: "normal",
+      heightNum: 5,
+      scrollHeight: 0,
+    },
+    {
+      // 2
+      type: "sticky",
+      heightNum: 5,
+      scrollHeight: 0,
+    },
+    {
+      // 3
+      type: "sticky",
+      heightNum: 5,
+      scrollHeight: 0,
+    },
+  ];
+
+  function setLayout() {}
+})();
