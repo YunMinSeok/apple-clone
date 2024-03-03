0f216f992dda6f855d0e60f061d30a1c3a5414bf
윤민석 <ykmvm147@naver.com>
2024-03-03 14:08:07 +0900

    feat: 현재 활성 씬 반영하기 완료

--- js/blank.js
+++ js/blank.js
@@ -51,6 +51,18 @@
         i
       ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
+
+    yOffset = window.pageYOffset;
+
+    let totalScrollHeight = 0;
+    for (let i = 0; i < sceneInfo.length; i++) {
+      totalScrollHeight += sceneInfo[i].scrollHeight;
+      if (totalScrollHeight >= yOffset) {
+        currentScene = i;
+        break;
+      }
+    }
+    document.body.setAttribute("id", `show-scene-${currentScene}`);
   }
 
   function scrollLoop() {
@@ -61,21 +73,20 @@
 
     if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
       currentScene++;
+      document.body.setAttribute("id", `show-scene-${currentScene}`);
     }
 
     if (yOffset < prevScrollHeight) {
-      if (currentScene === 0) {
-        return;
-      }
+      if (currentScene === 0) return;
       currentScene--;
+      document.body.setAttribute("id", `show-scene-${currentScene}`);
     }
-    console.log(currentScene);
   }
 
-  window.addEventListener("resize", setLayout);
   window.addEventListener("scroll", () => {
     yOffset = window.pageYOffset;
     scrollLoop();
   });
-  setLayout();
+  window.addEventListener("load", setLayout);
+  window.addEventListener("resize", setLayout);
 })();
