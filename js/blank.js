b065b63a5ef934f845a7ac83c91258c003a93568
윤민석 <ykmvm147@naver.com>
2024-03-08 19:34:29 +0900

    feat: 폰트 등장시 translateY값 추가 완료

--- js/blank.js
+++ js/blank.js
@@ -19,8 +19,9 @@
       },
       values: {
         messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
-        messageB_opacity_in: [0, 1, { start: 0.3, end: 0.4 }],
+        messageA_translateY_in: [20, 0, { start: 0.1, end: 0.2 }],
         messageA_opacity_out: [1, 0, { start: 0.25, end: 0.3 }],
+        messageA_translateY_out: [0, -20, { start: 0.25, end: 0.3 }],
       },
     },
     {
@@ -122,10 +123,20 @@
           values.messageA_opacity_out,
           currentYOffset
         );
+        const messageA_translateY_in = calcValues(
+          values.messageA_translateY_in,
+          currentYOffset
+        );
+        const messageA_translateY_out = calcValues(
+          values.messageA_translateY_out,
+          currentYOffset
+        );
         if (scrollRatio <= 0.22) {
           objs.messageA.style.opacity = messageA_opacity_in;
+          objs.messageA.style.transform = `translateY(${messageA_translateY_in}%)`;
         } else {
           objs.messageA.style.opacity = messageA_opacity_out;
+          objs.messageA.style.transform = `translateY(${messageA_translateY_out}%)`;
         }
         break;
       case 1:
