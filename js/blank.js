613bff3c9cb73eccd10125a708cd0a160f4e6eac
윤민석 <ykmvm147@naver.com>
2024-04-15 20:47:08 +0900

    feat: 스크롤에 반응하는 메뉴 만들기 완료

--- js/blank.js
+++ js/blank.js
@@ -146,6 +146,15 @@
     }
   }
   setCanvasImages();
+
+  function checkMenu() {
+    if (yOffset > 44) {
+      document.body.classList.add("local-nav-sticky");
+    } else {
+      document.body.classList.remove("local-nav-sticky");
+    }
+  }
+
   function setLayout() {
     for (let i = 0; i < sceneInfo.length; i++) {
       if (sceneInfo[i].type === "sticky") {
@@ -615,6 +624,7 @@
   window.addEventListener("scroll", () => {
     yOffset = window.pageYOffset;
     scrollLoop();
+    checkMenu();
   });
   window.addEventListener("load", () => {
     setLayout();
