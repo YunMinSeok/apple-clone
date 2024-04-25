6e09681c369cb171f22720e41ed15fe8fb8ae63a
윤민석 <ykmvm147@naver.com>
2024-04-25 22:42:18 +0900

    feat: 자잘한 버그 수정

--- js/blank.js
+++ js/blank.js
@@ -671,8 +671,8 @@
     window.addEventListener("resize", () => {
       if (window.innerWidth > 900) {
         setLayout();
+        sceneInfo[3].values.rectStartY = 0;
       }
-      sceneInfo[3].values.rectStartY = 0;
     });
 
     window.addEventListener("orientationchange", () => {
